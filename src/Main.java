a6cc672b572152a078d0397e3ca17cb414ea3e42
eu020494myv <yuliia.yhashko@monobank.ua>
2023-12-03 20:17:19 +0200

    2. Java Basics: Data types, Operators and Variables

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
-    private static Scanner scanner;
-//    static int two(int result2){
-//        return result2;
-//    }
-//    static int three(int result3){
-//        return result3;
-//    } - тут я пробувала запустити методи з мейну (Повторить задание 2, но выполнить его в отдельных методах и запустить эти методы в методе Main)
-
 
     public static void main(String[] args) {
 
+        //first task
         int myInt = 20398290; //type variableName = value;
         short myShort = 32500;
         byte myByte = 120;
@@ -21,34 +14,44 @@ public class Main {
         boolean myBoolean = true;
         char myChar = 'q';
 
-        {
-            System.out.println(myInt + myByte);
-            System.out.println(myLong - myFloat);
-            System.out.println(myByte * myDouble);
-            System.out.println(myLong / myShort);
-            System.out.println(myInt % myShort);
+        //second task
+        System.out.println(myInt + myByte);
+        System.out.println(myLong - myFloat);
+        System.out.println(myByte * myDouble);
+        System.out.println(myLong / myShort);
+        System.out.println(myInt % myShort);
+
+        //third task
+        minus();
+        divide();
+        //fourth task
+        scanner();
+    }
+
+    public static void minus() {
+        int a = 400;
+        short b = 850;
+        int result = b - a;
+        System.out.println(result);
+    }
+
+    public static void divide() {
+        byte a = 39;
+        long b = 666888;
+        long result = b / a;
+        System.out.println(result);
+    }
+
+    public static void scanner(){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("please provide a number");
+        int number = reader.nextInt();
+        if (number % 2 == 0) {
+            System.out.println(number + " is even");
+        } else {
+            System.out.println(number + " is odd");
         }
-    }
 
-    public void thirdTask() {
-        int anotherInt = 400;
-        short anotherShort = 850;
-        int result2 = anotherShort - anotherInt;
     }
-
-    public void thirdTask2() {
-        byte anotherByte = 39;
-        long anotherLong = 666888;
-        long result3 = anotherLong / anotherByte;
-    }
-//        public static void scanner(){
-//            Scanner reader = new Scanner(System.in);
-//            int number = scanner.nextInt();
-//            if (number % 2 == 0)
-//                System.out.println(number + "even");
-//            else System.out.println(number + "odd");
-//
-//        } - тут я пробувала виконати завданя С помощью класса Scanner ввести целое число. Далее создайте условие, если это число четное то выведите сообщение "Четное число", если нет то "Не четное число"
-
 
     }
